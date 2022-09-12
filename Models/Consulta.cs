a41807dcb5aeaccb5da35a3cc7fcbf5df8b0baaa
Paloma Souza <paloma.souza112@gmail.com>
2022-09-12 18:52:11 -0300

    Model Consulta criado

--- Models/Consulta.cs
+++ Models/Consulta.cs
@@ -1,6 +1,22 @@
-﻿namespace ConsultaMedicaVet.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsultaMedicaVet.Models
 {
     public class Consulta
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime DataHora { get; set; }
+
+        [ForeignKey("Medico")]
+        public int IdMedico { get; set; }
+        public Medico Medico { get; set; }
+
+        [ForeignKey("Paciente")]
+        public int IdPaciente { get; set; }
+        public Paciente Paciente { get; set; }
     }
 }
