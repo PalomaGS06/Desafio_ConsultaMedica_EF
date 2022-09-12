7910822b57c1f6b1d4c6a3a8e8ebda5e31056d1a
Paloma Souza <paloma.souza112@gmail.com>
2022-09-12 19:18:42 -0300

    Configuração do Sql Server e context adicionados no Startup

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using ConsultaMedicaVet.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,11 @@ namespace ConsultaMedicaVet
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ConsultaMedicaVet", Version = "v1" });
             });
+
+            services.AddDbContext<ConsultaMedVetContext>( options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"))
+            );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
