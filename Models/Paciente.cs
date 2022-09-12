d8b75cd196dbcaa219ef83a92ebe5c8d97cec5ce
Paloma Souza <paloma.souza112@gmail.com>
2022-09-12 18:39:03 -0300

    Model Paciente criado

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -1,6 +1,21 @@
-﻿namespace ConsultaMedicaVet.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsultaMedicaVet.Models
 {
     public class Paciente
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Carteirinha { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int IdUsuario { get; set; }
+        public Usuario Usuario { get; set; }
+
     }
 }
