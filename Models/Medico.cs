93268dc529e675bb029f6d03a276b2562201f90a
Paloma Souza <paloma.souza112@gmail.com>
2022-09-12 18:43:41 -0300

    Model Medico criado

--- Models/Medico.cs
+++ Models/Medico.cs
@@ -1,6 +1,21 @@
-﻿namespace ConsultaMedicaVet.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsultaMedicaVet.Models
 {
     public class Medico
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CRM { get; set; }
+
+        [ForeignKey("Especialidade")]
+        public int IdEspecialidade { get; set; }
+        public Especialidade Especialidade { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int IdUsuario { get; set; }
+        public Usuario Usuario { get; set; }
     }
 }
