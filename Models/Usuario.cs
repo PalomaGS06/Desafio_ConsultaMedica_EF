12b255337c08919e662cd500ff57dfe1366ff82e
Paloma Souza <paloma.souza112@gmail.com>
2022-09-12 18:41:11 -0300

    Model Usuario criado

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,6 +1,18 @@
-﻿namespace ConsultaMedicaVet.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsultaMedicaVet.Models
 {
     public class Usuario
     {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        [ForeignKey("TipoUsuario")]
+        public int IdTipoUsuario { get; set; }
+        public TipoUsuario TipoUsuario { get; set; }
     }
 }
