902fbbceb8aa2836943c254d44a38482f7d5cb8c
Paloma Souza <paloma.souza112@gmail.com>
2022-09-13 09:56:31 -0300

    Interface do Usuario gerada

--- Interfaces/IUsuarioRepository.cs
+++ Interfaces/IUsuarioRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace ConsultaMedicaVet.Interfaces
+﻿using ConsultaMedicaVet.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
+
+namespace ConsultaMedicaVet.Interfaces
 {
     public interface IUsuarioRepository
     {
+        Usuario Inserir(Usuario usuario);
+        ICollection<Usuario> ListarTodosUsers();
+        Usuario BuscarPorId(int id);
+        void Alterar(Usuario usuario);
+        void Excluir(Usuario usuario);
+        void AlterarParcialmente(JsonPatchDocument patchUsuario, Usuario usuario);
     }
 }
