ab93953828188b9654fee4a0f102a171433b6561
Paloma Souza <paloma.souza112@gmail.com>
2022-09-13 10:04:19 -0300

    Interface Especialidade gerada

--- Interfaces/IEspecialidadeRepository.cs
+++ Interfaces/IEspecialidadeRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace ConsultaMedicaVet.Interfaces
+﻿using ConsultaMedicaVet.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
+
+namespace ConsultaMedicaVet.Interfaces
 {
     public interface IEspecialidadeRepository
     {
+        Especialidade Inserir(Especialidade especialidade);
+        ICollection<Especialidade> ListarTodas();
+        Especialidade BuscarPorId(int id);
+        void Alterar(Especialidade especialidade);
+        void Excluir(Especialidade especialidade);
+        void AlterarParcialmente(JsonPatchDocument patchEspecialidade, Especialidade especialidade);
     }
 }
