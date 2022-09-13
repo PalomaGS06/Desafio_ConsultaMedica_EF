43e30c01e94d5f49c71e501898a1ef54bfa25353
Paloma Souza <paloma.souza112@gmail.com>
2022-09-13 09:43:01 -0300

    Interface TipoUsuario gerada

--- Interfaces/ITipoUsuarioRepository.cs
+++ Interfaces/ITipoUsuarioRepository.cs
@@ -6,6 +6,12 @@ namespace ConsultaMedicaVet.Interfaces
 {
     public interface ITipoUsuarioRepository
     {
+        TipoUsuario Inserir(TipoUsuario tipoUsuario);
+        ICollection<TipoUsuario> ListarTodos();
+        TipoUsuario BuscarPorId(int id);
+        void Alterar(TipoUsuario tipoUsuario);
+        void Excluir(TipoUsuario tipoUsuario);
+        void AlterarParcialmente(JsonPatchDocument patchTipoUsuario, TipoUsuario tipoUsuario);
 
     }
 }
