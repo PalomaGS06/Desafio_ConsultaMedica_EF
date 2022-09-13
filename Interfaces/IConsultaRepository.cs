74438a8d744f6ab5c718e7984bc02eb99bbf27d6
Paloma Souza <paloma.souza112@gmail.com>
2022-09-13 10:06:30 -0300

    Interface Consulta gerada

--- Interfaces/IConsultaRepository.cs
+++ Interfaces/IConsultaRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace ConsultaMedicaVet.Interfaces
+﻿using ConsultaMedicaVet.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
+
+namespace ConsultaMedicaVet.Interfaces
 {
     public interface IConsultaRepository
     {
+        Consulta Inserir(Consulta consultas);
+        ICollection<Consulta> ListarTodas();
+        Consulta BuscarPorId(int id);
+        void Alterar(Consulta consultas);
+        void Excluir(Consulta consultas);
+        void AlterarParcialmente(JsonPatchDocument patchConsulta, Consulta consultas);
     }
 }
