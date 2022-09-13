95192defe3be2ae4f5c79c67b11a368fc6a1e709
Paloma Souza <paloma.souza112@gmail.com>
2022-09-13 09:59:17 -0300

    Interface Paciente gerada

--- Interfaces/IPacienteRepository.cs
+++ Interfaces/IPacienteRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace ConsultaMedicaVet.Interfaces
+﻿using ConsultaMedicaVet.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
+
+namespace ConsultaMedicaVet.Interfaces
 {
     public interface IPacienteRepository
     {
+        Paciente Inserir(Paciente paciente);
+        ICollection<Paciente> ListarTodos();
+        Paciente BuscarPorId(int id);
+        void Alterar(Paciente paciente);
+        void Excluir(Paciente paciente);
+        void AlterarParcialmente(JsonPatchDocument patchPaciente, Paciente paciente);
     }
 }
