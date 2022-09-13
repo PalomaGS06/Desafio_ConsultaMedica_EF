3b7b97aebf75d3d89c6a7d01cecbe85a4fe7fc84
Paloma Souza <paloma.souza112@gmail.com>
2022-09-13 10:02:22 -0300

    Interface Medico gerada

--- Interfaces/IMedicoRepository.cs
+++ Interfaces/IMedicoRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace ConsultaMedicaVet.Interfaces
+﻿using ConsultaMedicaVet.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
+
+namespace ConsultaMedicaVet.Interfaces
 {
     public interface IMedicoRepository
     {
+        Medico Inserir(Medico medico);
+        ICollection<Medico> ListarTodos();
+        Medico BuscarPorId(int id);
+        void Alterar(Medico medico);
+        void Excluir(Medico medico);
+        void AlterarParcialmente(JsonPatchDocument patchMedico, Medico medico);
     }
 }
