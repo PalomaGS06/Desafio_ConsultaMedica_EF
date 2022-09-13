1ed3d8391c0831b17cdd4345f7d905c192e0f5e4
Paloma Souza <paloma.souza112@gmail.com>
2022-09-13 17:34:11 -0300

    PacienteRepository implementado

--- Repositories/PacienteRepository.cs
+++ Repositories/PacienteRepository.cs
@@ -1,6 +1,55 @@
-﻿namespace ConsultaMedicaVet.Repositories
+﻿using ConsultaMedicaVet.Contexts;
+using ConsultaMedicaVet.Interfaces;
+using ConsultaMedicaVet.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsultaMedicaVet.Repositories
 {
-    public class PacienteRepository
+    public class PacienteRepository : IPacienteRepository
     {
+        // Injeção de Dependência
+        ConsultaMedVetContext ctx; //criando a dependência de contexto
+        public PacienteRepository(ConsultaMedVetContext _ctx) //método construtor
+        {
+            ctx = _ctx;
+        }
+        public void Alterar(Paciente paciente)
+        {
+            ctx.Entry(paciente).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+
+        public void AlterarParcialmente(JsonPatchDocument patchPaciente, Paciente paciente)
+        {
+            patchPaciente.ApplyTo(paciente);
+            ctx.Entry(paciente).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+
+        public Paciente BuscarPorId(int id)
+        {
+            return ctx.Paciente.Find(id);
+        }
+
+        public void Excluir(Paciente paciente)
+        {
+            ctx.Paciente.Remove(paciente);
+            ctx.SaveChanges();
+        }
+
+        public Paciente Inserir(Paciente paciente)
+        {
+            ctx.Paciente.Add(paciente);
+            ctx.SaveChanges();
+            return paciente;
+        }
+
+        public ICollection<Paciente> ListarTodos()
+        {
+            return ctx.Paciente.ToList();
+        }
     }
 }
