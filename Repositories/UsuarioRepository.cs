ee232b6d7f504a9ab81a67a7137bc8fff4c8e999
Paloma Souza <paloma.souza112@gmail.com>
2022-09-13 17:25:05 -0300

    Repository do Usuario implementado

--- Repositories/UsuarioRepository.cs
+++ Repositories/UsuarioRepository.cs
@@ -1,6 +1,56 @@
-﻿namespace ConsultaMedicaVet.Repositories
+﻿using ConsultaMedicaVet.Contexts;
+using ConsultaMedicaVet.Interfaces;
+using ConsultaMedicaVet.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsultaMedicaVet.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository : IUsuarioRepository
     {
+        // Injeção de Dependência
+        ConsultaMedVetContext ctx; //criando a dependência de contexto
+        public UsuarioRepository(ConsultaMedVetContext _ctx) //método construtor
+        {
+            ctx = _ctx;
+        }
+
+        public void Alterar(Usuario usuario)
+        {
+            ctx.Entry(usuario).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+
+        public void AlterarParcialmente(JsonPatchDocument patchUsuario, Usuario usuario)
+        {
+            patchUsuario.ApplyTo(usuario);
+            ctx.Entry(usuario).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+
+        public Usuario BuscarPorId(int id)
+        {
+            return ctx.Usuarios.Find(id);
+        }
+
+        public void Excluir(Usuario usuario)
+        {
+            ctx.Usuarios.Remove(usuario);
+            ctx.SaveChanges();
+        }
+
+        public Usuario Inserir(Usuario usuario)
+        {
+            ctx.Usuarios.Add(usuario);
+            ctx.SaveChanges();
+            return usuario;
+        }
+
+        public ICollection<Usuario> ListarTodosUsers()
+        {
+            return ctx.Usuarios.ToList();
+        }
     }
 }
