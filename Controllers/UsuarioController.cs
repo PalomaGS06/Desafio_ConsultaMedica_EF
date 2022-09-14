1a3db1a909dce733e146ff7c6400f83592ee50f1
Paloma Souza <paloma.souza112@gmail.com>
2022-09-13 21:40:38 -0300

    Controller de usuário gerado

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ConsultaMedicaVet.Interfaces;
+using ConsultaMedicaVet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConsultaMedicaVet.Controllers
@@ -7,5 +10,173 @@ namespace ConsultaMedicaVet.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        // Injeção de dependência do repositório
+        private readonly IUsuarioRepository repositorio;
+
+        public UsuarioController(IUsuarioRepository _repositorio) //criando um construtor
+        {
+            repositorio = _repositorio;
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+
+            try
+            {
+                var retorno = repositorio.Inserir(usuario);
+                return Ok(retorno);
+
+            }
+            catch (System.Exception e)
+            {
+                // return BadRequest(e.Message);            
+                return StatusCode(500, new
+                {
+                    Error = "Falha na transação !!",
+                    Message = e.Message,
+                });
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                var retorno = repositorio.ListarTodosUsers();
+                return Ok(retorno);
+
+            }
+            catch (System.Exception e)
+            {
+                return StatusCode(500, new
+                {
+                    Error = "Falha de transação !!",
+                    Message = e.Message,
+                });
+
+            }
+
+        }
+        [HttpGet("{id}")]
+        public IActionResult BuscarUsuarioPorID(int id)
+        {
+            try
+            {
+                var retorno = repositorio.BuscarPorId(id);
+                if (retorno == null)
+                {
+                    return NotFound(new
+                    {
+                        Message = "Usuario não achado na lista !!"
+                    });
+                }
+
+                return Ok(retorno);
+
+            }
+            catch (System.Exception e)
+            {
+                return StatusCode(500, new
+                {
+                    Error = "Falha na transação !!",
+                    Message = e.Message,
+                });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar(int id, Usuario usuario)
+        {
+            try
+            {
+
+                //Verificar se os ids batem!
+                if (id != usuario.Id)
+                {
+                    return BadRequest();
+                }
+
+                //Verificar se o id existe no banco!
+                var retorno = repositorio.BuscarPorId(id);
+                if (retorno == null)
+                {
+                    return NotFound(new
+                    {
+                        Message = "Usuario não encontrado !!"
+                    });
+                }
+
+                //Altera efetivamente o usuário!
+                repositorio.Alterar(usuario);
+
+                return NoContent();
+
+            }
+            catch (System.Exception e)
+            {
+                return StatusCode(500, new
+                {
+                    Error = "Falha na transação !!",
+                    Message = e.Message,
+                });
+            }
+
+        }
+
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, [FromBody] JsonPatchDocument patchUsuario)
+        {
+            if (patchUsuario == null)
+            {
+                return BadRequest();
+            }
+
+            // Temos que buscar o objeto
+            var usuario = repositorio.BuscarPorId(id); //usuario encontrado
+            if (usuario == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Usuário não encontrado !!"
+                });
+            }
+
+            repositorio.AlterarParcialmente(patchUsuario, usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            try
+            {
+                var busca = repositorio.BuscarPorId(id);
+                if (busca == null)
+                {
+                    return NotFound(new
+                    {
+                        Message = "Usuário não encontrado !!"
+                    });
+                }
+
+                repositorio.Excluir(busca);
+
+                return NoContent(); // Status 204 de sucesso
+
+            }
+            catch (System.Exception e)
+            {
+                return StatusCode(500, new
+                {
+                    Error = "Falha na transação!!",
+                    Message = e.Message,
+                });
+            }
+
+        }
     }
 }
